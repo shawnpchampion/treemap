56af3d22326abc1f117b06b53125ff271cc87b36
shawnpchampion <shawnpchampion@gmail.com>
2021-10-03 14:57:27 -1000

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -285,8 +285,8 @@ $.getJSON("data/DOITT_MUSEUM_01_13SEPT2010.geojson", function (data) {
 });
 
 map = L.map("map", {
-  zoom: 10,
-  center: [40.702222, -73.979378],
+  zoom: 16,
+  center: [19.40893, -154.914],
   layers: [cartoLight, markerClusters, highlight],
   zoomControl: false,
   attributionControl: false
