bff46fdfb1e3015837491ddce0fd5c7930b6d46c
shawnpchampion <shawnpchampion@gmail.com>
2021-10-05 17:58:21 -1000

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -249,7 +249,7 @@ var museums = L.geoJson(null, {
     return L.marker(latlng, {
       icon: L.icon({
         iconUrl: "assets/img/museum.png",
-        iconSize: [24, 28],
+        iconSize: [34, 34],
         iconAnchor: [12, 28],
         popupAnchor: [0, -25]
       }),
@@ -397,7 +397,7 @@ var baseLayers = {
 
 var groupedOverlays = {
   "Points of Interest": {
-    "<img src='assets/img/theater.png' width='44' height='40'>&nbsp;Theaters": theaterLayer, //sizes for control box
+    "<img src='assets/img/theater.png' width='24' height='24'>&nbsp;Theaters": theaterLayer, //sizes for control box
     "<img src='assets/img/museum.png' width='24' height='28'>&nbsp;Museums": museumLayer
   },
   "Reference": {
