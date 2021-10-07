9dd8bccfe4d79c5dcd1550b60b0faa22cd69f069
shawnpchampion <shawnpchampion@gmail.com>
2021-10-06 23:00:50 -1000

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -294,13 +294,13 @@ $.getJSON("data/DOITT_MUSEUM_01_13SEPT2010.geojson", function (data) {
 });
 
 // Add background image to point-marker pop up
-//var bgimgurl = "https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823_1280.jpg";
+var bgimgurl = "https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823_1280.jpg";
 //var bgimgurl = "./assets/img/uluback.png";
-var bgimgurl = 'url(' + feature.properties.backimage + ')';
+//var bgimgurl = 'url(' + feature.properties.backimage + ')';
 var div = document.getElementById("bgimage");
 //div.style.backgroundImage = `url(${feature.properties.backimage})`;
-//div.style.backgroundImage = `url(${bgimgurl})`;
-div.style.backgroundImage = bgimgurl;
+div.style.backgroundImage = `url(${bgimgurl})`;
+//div.style.backgroundImage = bgimgurl;
 // div.style.width = "640px";
 // div.style.height = "100%";
 
