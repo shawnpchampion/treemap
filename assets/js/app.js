6598391e35589fe885b9fe5147105b7e4424906c
shawnpchampion <shawnpchampion@gmail.com>
2021-10-06 21:27:07 -1000

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -80,10 +80,11 @@ function sidebarClick(id) {
   }
 }
 // Add background image to point-marker pop up
-var bgimgurl = "https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823_1280.jpg";
+//var bgimgurl = "https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823_1280.jpg";
 //var bgimgurl = "./assets/img/uluback.png";
+var bgimgurl = feature.properties.backimage;
 var div = document.getElementById("bgimage");
-// div.style.backgroundImage = `url(${feature.properties.backimage})`;
+// div.style.backgroundImage = `url(${feature.properties.backimage})`; did not work
 div.style.backgroundImage = `url(${bgimgurl})`;
 // div.style.width = "640px";
 // div.style.height = "100%";
@@ -171,7 +172,7 @@ var theaterLayer = L.geoJson(null);
 var theaters = L.geoJson(null, {
   pointToLayer: function (feature, latlng) {
     return L.marker(latlng, {
-      icon: L.icon({
+        icon: L.icon({
         iconUrl: "assets/img/theater.png",
         iconSize: [34, 34],                                //map sizing
         iconAnchor: [12, 28],
